5e6d2ee59c0ef6bec91fb23533b0e90c3e794811
DevExpressExampleBot <fakeEmail>
2022-12-18 20:20:40 +0400

    Source auto update [skip ci]

--- VB/TreeMapHierarchicalDataAdapterSample/DataModel.vb
+++ VB/TreeMapHierarchicalDataAdapterSample/DataModel.vb
@@ -1,25 +1,24 @@
-﻿Imports System
 Imports System.Collections.Generic
-Imports System.Collections.ObjectModel
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 
 Namespace TreeMapHierarchicalDataAdapterSample
-	Public Class Country
-		Public Property Name() As String
-		Public Property Gdp() As Double
-	End Class
 
-	Public Class SideOfWorld
-		Public Property Name() As String
+    Public Class Country
 
-'INSTANT VB NOTE: The field countries was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private countries_Conflict As New List(Of Country)()
-		Public ReadOnly Property Countries() As List(Of Country)
-			Get
-				Return countries_Conflict
-			End Get
-		End Property
-	End Class
+        Public Property Name As String
+
+        Public Property Gdp As Double
+    End Class
+
+    Public Class SideOfWorld
+
+        Public Property Name As String
+
+        Private countriesField As List(Of Country) = New List(Of Country)()
+
+        Public ReadOnly Property Countries As List(Of Country)
+            Get
+                Return countriesField
+            End Get
+        End Property
+    End Class
 End Namespace
